5bfc06dfaa55a51d6a179dcd4200c918e902a222
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-11-05 15:05:05 -0800

    good progress

--- DataFun/DataFun.java
+++ DataFun/DataFun.java
@@ -1,12 +1,110 @@
-// any imports here
+import java.util.Scanner;
 
 /**
-     * Description goes here
-     * @author [Student Name]
-     * @version [Current Date]
-     */
+ * Project 2: DataFun
+ * @ Reed Jordan
+ * AP Comp Sci A w/ Mr. Smith
+ * Started 11/5/2024
+ * Ended --/--/--
+ */
+
+
 public class DataFun{
     public static void main(String[] args){
-        
+        //favoriteNumberActivities();
+        favoriteCharacterStuff();
+
     }
+
+    public static void favoriteNumberActivities(){
+
+
+        System.out.println("What is your favorite number?");
+        Scanner input = new Scanner(System.in);
+        int favNum = input.nextInt();
+
+        if (favNum < 0){
+            System.out.print(favNum +" is negative,");
+        }else{
+            System.out.print(favNum +" is positive,");
+        }
+
+        if (favNum > 1000){
+            System.out.print(" is greater than 1000,");
+        }else if(favNum > 100){
+            System.out.print(" is greater than 100,");
+        }else if(favNum > 10){
+            System.out.print(" is greater than 10,");
+        }
+
+        if (favNum % 2 == 0){
+            System.out.print(" and is even. ");
+        }else{
+            System.out.print(" and is odd. ");
+        }
+
+        if (favNum == 2){
+            System.out.println("It is also the atomic number of the noble gas Helium.");
+        }else if (favNum == 10){
+            System.out.println("It is also the atomic number of the noble gas Neon.");
+        }else if (favNum == 18){
+            System.out.println("It is also the atomic number of the noble gas Argon.");
+        }else if (favNum == 36){
+            System.out.println("It is also the atomic number of the noble gas Krypton.");
+        }else if (favNum == 54){
+            System.out.println("It is also the atomic number of the noble gas Xenon.");
+        }else if (favNum == 86){
+            System.out.println("It is also the atomic number of the noble gas Radon.");
+        }else{
+            System.out.println("It is also not the number of any atomic gases.");
+        }
+    }
+
+    public static void favoriteCharacterStuff(){
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Look at the keyboard and find your favorite character. Is it a:"+
+                "\n\t1- Number\n\t2- Letter");
+        int userChoice = input.nextInt();
+
+        int numFav = Integer.MIN_VALUE;
+        String charaFav = null;
+
+        switch (userChoice){
+            case 1:
+                System.out.println("Please enter your favorite number:");
+                numFav = input.nextInt();
+                break;
+            case 2:
+                System.out.println("Please enter your favorite character:");
+                charaFav = input.nextLine();
+                break;
+        }
+
+        if(numFav != Integer.MIN_VALUE){
+
+        }
+        if(!(charaFav.equals(null))){
+            char ch = charaFav.charAt(0);
+            int asciiVal = ch;
+            System.out.print("The ascii value of your character is "+ asciiVal);
+            if(asciiVal >= 65 && asciiVal <= 90){
+                System.out.print(". Your character is an uppercase letter ");
+                if (asciiVal == 65 || asciiVal == 69 || asciiVal == 73 || asciiVal == 79 || asciiVal == 85 || asciiVal == 89){
+                    System.out.print("and a vowel.");
+                }else{
+                    System.out.print("and a consonant.");
+                }
+            }else if (asciiVal >= 97 && asciiVal <= 122){
+                System.out.print(". Your character is an lowercase letter ");
+                if (asciiVal == 97 || asciiVal == 101 || asciiVal == 105 || asciiVal == 111 || asciiVal == 117 || asciiVal == 122){
+                    System.out.print("and a vowel.");
+                }else{
+                    System.out.print("and a consonant.");
+                }
+        }
+
+    }
+
 }
--- DataFun/README.md
+++ DataFun/README.md
@@ -1,11 +1,13 @@
 # Data Fun - Project 2
 
-The purpose of this assignment is to get some experience working with selection structures (if, else, elif, and switch cases), as well as more experience working with simple data types. You might need to do some
-reserach and some of the words are links so you can see the Wikipedia page.
+The purpose of this assignment is to get some experience working with selection structures (if, else, elif, and switch cases), 
+as well as more experience working with simple data types. You might need to do some research and some of the words are links 
+so you can see the Wikipedia page.
 
 ## Overview
 
-This program will allow you to demonstrate your mastery of ifs, elses, elif, and switch cases.   It will consist of two distinct parts: one that deals with integers and one that deals with characters.
+This program will allow you to demonstrate your mastery of ifs, elses, elif, and switch cases.   It will consist of two distinct 
+parts: one that deals with integers and one that deals with characters.
 
 The program will use the command line for its input and output (I/O).
 
@@ -15,7 +17,8 @@ The first part of the program will do the following:
 - Print out if it’s positive or negative (consider 0 as positive)
 - Print out if it’s greater than 10, 100, or 1000 (only print the highest one it’s greater than).
 - Print out if it’s odd or even
-- If the number is the atomic number of one of the 7 noble gases, it should print out the name of the [noble gas](https://en.wikipedia.org/wiki/Noble_gas) corresponding to that atomic number.  If the number is NOT the atomic number of one of the 7 noble gases, it should print a message stating that.  (use match case for this)
+- If the number is the atomic number of one of the 7 noble gases, it should print out the name of the [noble gas](https://en.wikipedia.org/wiki/Noble_gas) corresponding to that atomic number.  
+  -   If the number is NOT the atomic number of one of the 7 noble gases, it should print a message stating that.  (use match case for this)
 
 The second part of the program will do the following:
 - Get the user’s favorite character.
@@ -54,13 +57,12 @@ Output should be well formatted and look similar to this:
 ## Technical Details
 - Name your program DataFun.java
 - Include a block at the top of your code that has your name, date, and description of the what the program does.
-- You will be turning in pseudocode for this. It will be attached to the assignment as a separate Google Doc
 - You must use ifs, elses, and at least one switch case
   
 As with all programs written in this course, maintainability is as important as functionality, so your code should be clear and easy to follow.
 
 - Google's Java Style Guide: [https://google.github.io/styleguide/javaguide.html](https://google.github.io/styleguide/javaguide.html)
-- Speficially Sections 2.1, 3.3, 4, and 5
+- Specifically Sections 2.1, 3.3, 4, and 5
 
 ## Submitting
 
